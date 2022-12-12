6512e796469c2d369a899457eca4647883e72b1a
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2022-12-11 22:47:50 -0500

    fixed some spacing, adjusted the IF statements

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var generateBtn = document.querySelector("#generate");
 var passwordLength = 8 - 128;
-var pWord="";
+var pWord=[];
 var specialChar = "!@#$%^&*()";
 var number = "0123456789";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -8,31 +7,33 @@ var UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var generateBtn = document.querySelector("#generate");
 
+function writePassword(){
+    var correctPW= getPW();
+    var passwordText = document.querySelector("#password");
+
+    if (correctPW){
+        var newPassword = generatePassword();
+        passwordText.value = newPassword;
+    }else{
+        passwordText.value = "";
+    }
+    
+}
+
+
 function generatePassword() {
-    var password = "";
+    var password= "";
     for (var i = 0; i < passwordLength; i++) {
-        var randomnumber = Math.floor(Math.random() * pWord.length);
-        password = password + pWord;[randomnumber];
+         var randomWord = Math.floor(Math.random() * password.length);
+            password = password + pWord[randomWord];
     }
     return password;
 }
 
-// Add event listener to generate button
 
-generateBtn.addEventListener("click", writePassword);
-
-
-function writePassword(){
-    var newpassword= generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value =newpassword;
-
-
-}
-
-
-function generatePassword() {
+function getPW() {
      pWord="";
+     
      passwordLength = parseInt(prompt("How many characters do you want your password to include? 8 - 128 characters"));
 
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
@@ -42,22 +43,30 @@ function generatePassword() {
 
     if (confirm("Would you like to use special characters?")){
         pWord = pWord.concat(specialChar);
+       
     }
 
     if (confirm("Would you like to use numbers?")) {
         pWord = pWord.concat(number);
+       
 
     }
 
     if (confirm("Would you like to use lowercase letters?")) {
         pWord = pWord.concat(lowercase);
+       
 
     }
 
     if (confirm("Would you like to use UPPERCASE letters?")) {
         pWord = pWord.concat(UPPERCASE);
+       
 
     }
 
-    return true;
+    return password;
 }
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
